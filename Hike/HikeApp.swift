b0571dc431e4f946c5b1b4ff4ed7a4a1833eedae
Rishop Babu <rishopbabu22@gmail.com>
2024-11-12 21:19:17 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Rishop Babu on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
